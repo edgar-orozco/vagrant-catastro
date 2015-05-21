3d8309be9a2f2411d16319054845632ada49af34
edgar-orozco <edgar.orozco@bxd.mx>
2015-05-21 18:57:28 -0500

    Update bootstrap-prod.sh

--- bootstrap-prod.sh
+++ bootstrap-prod.sh
@@ -109,16 +109,16 @@ chown apache.apache /var/www/html/app/storage -R
 chmod 775 /var/www/html/app/storage -R
 # Se cambian permisos para carpeta web donde se escriben algunos archivos
 chown apache.apache /var/www/html/public/css/images/logos -R
-chmod 775 /var/www/html/public/css/images/logos -R
+chmod 755 /var/www/html/public/css/images/logos -R
 #se cambian permisos para directorio de datos complementarios
 chown apache.apache /var/www/html/public/complementarios/anexos -R
-chmod 775 /var/www/html/public/complementarios/anexos -R
+chmod 755 /var/www/html/public/complementarios/anexos -R
 #se cambian permisos para directorio de documentos requisitos en tramites
 chown apache.apache /var/www/html/documentos -R
-chmod 775 /var/www/html/public/documentos -R
+chmod 755 /var/www/html/public/documentos -R
 #se cambian permisos para directorio de documentos de predios valuados (corevat)
 chown apache.apache /var/www/html/corevat -R
-chmod 775 /var/www/html/public/corevat -R
+chmod 755 /var/www/html/public/corevat -R
 
 
 # Configurando apache
